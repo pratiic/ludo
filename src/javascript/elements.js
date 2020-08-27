292003d88b50a2c59c5001475190673e1587ce44
pratik bhandari <pratikbhandari9292@gmail.com>
2020-08-27 10:44:02 +0545

    added the dice

--- src/javascript/elements.js
+++ src/javascript/elements.js
@@ -1,15 +1,21 @@
 export let elements = {
 	colorHouses: document.querySelectorAll(".color-house"),
+
+	redRooms: document.querySelectorAll(".red-room"),
+	greenRooms: document.querySelectorAll(".green-room"),
+	blueRooms: document.querySelectorAll(".bleu-room"),
+	orangeRooms: document.querySelectorAll(".orange-room"),
+
 	tracks: document.querySelectorAll(".tracks"),
 	tracksHorizontal: document.querySelectorAll(".tracks-horizontal"),
 	tracksVertical: document.querySelectorAll(".tracks-vertical"),
 	tracksHorizontalIndividual: document.querySelectorAll(".track-horizontal"),
 	tracksVerticalIndividual: document.querySelectorAll(".track-vertical"),
 	tracksIndividual: document.querySelectorAll(".track"),
-	redRooms: document.querySelectorAll(".red-room"),
-	greenRooms: document.querySelectorAll(".green-room"),
-	blueRooms: document.querySelectorAll(".bleu-room"),
-	orangeRooms: document.querySelectorAll(".orange-room"),
+
 	brackets: document.querySelectorAll(".bracket"),
 	safeBrackets: document.querySelectorAll(".safe-bracket"),
+
+	dice: document.querySelector(".dice"),
+	faces: document.querySelectorAll(".face"),
 };
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -11,6 +11,10 @@ let safeBrackets = [
 	"bracket41",
 ];
 
+let diceRolled = 0;
+
+rollTheDice();
+
 elements.safeBrackets.forEach((bracket) => {
 	bracket.innerText = "x";
 });
@@ -20,3 +24,44 @@ elements.brackets.forEach((bracket) => {
 		bracket.innerText = "";
 	}
 });
+
+elements.dice.addEventListener("click", (event) => {
+	if (
+		event.target.classList.contains("face") ||
+		event.target.classList.contains("point")
+	) {
+		rollTheDice();
+	}
+});
+
+function rollTheDice() {
+	let faceId = `face${Math.ceil(Math.random() * 6)}`;
+
+	if (diceRolled !== 0) {
+		showAnimation();
+	}
+
+	setTimeout(function () {
+		elements.faces.forEach((face) => {
+			if (face.id === faceId) {
+				face.classList.add("turn");
+				face.classList.remove("hide");
+			} else {
+				face.classList.add("hide");
+				face.classList.remove("turn");
+			}
+		});
+
+		hideAnimation();
+	}, 900);
+
+	diceRolled++;
+}
+
+function showAnimation() {
+	elements.dice.style.animation = ` diceRollAnimation 300ms ease-in 3  `;
+}
+
+function hideAnimation() {
+	elements.dice.style.animation = "none";
+}
