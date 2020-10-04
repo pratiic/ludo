1b2ac9b6710d56326c10c2e0f111b65e76b2db27
pratik bhandari <pratikbhandari9292@gmail.com>
2020-10-04 18:53:32 +0545

    changed the animation to the first modal

--- javascript/modalAnimations.js
+++ javascript/modalAnimations.js
@@ -7,14 +7,18 @@ export function showFirstModal() {
 		.from(elements.beginGameModal, {
 			y: 350,
 			opacity: 0,
-			duration: 1,
-			ease: "power3.out",
+			duration: 0.75,
+			ease: "back.out(1.7)",
+		})
+		.to(elements.lettersInLudo, {
+			y: -20,
+			duration: 0.25,
+			stagger: 0.1,
+			ease: "power3.easeout",
 		})
 		.to(elements.lettersInLudo, {
 			y: 0,
-			opacity: 1,
 			duration: 0.3,
-			stagger: 0.2,
-			ease: "back.out(3)",
+			ease: "power3.easeout",
 		});
 }
